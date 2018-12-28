1af5095e0ee2d1b5047455d1cc48e4a2a45fd6ee
MTH <thornvik.mans@gmail.com>
2018-12-28 23:08:19 +0100

    Fixed 3.4.3 and validated the result

--- 3.4.3/script.js
+++ 3.4.3/script.js
@@ -1,34 +1,32 @@
 $(document).ready(function() {
-    // Alla länkar som pekar utanför den lokala sidan (med antingen http eller https) får target=_blank
-    $('a[href^=http]').each(function() {
-        $(this).attr('target', '_blank');
-    }); // end each
+  // Alla länkar som pekar utanför den lokala sidan (med antingen http eller https) får target=_blank
+  $('a[href^=http]').each(function() {
+    $(this).attr('target', '_blank');
+  }); // end each
 
-    // Göm menyn initiellt.
-    $('nav ul li ul').hide();
-    $('nav ul li ul li ul').hide();
+  // Göm menyn initiellt.
+  $('nav ul li ul').hide();
+  $('nav ul li ul li ul').hide();
 
-    // För första 'nivån' av menyn nav > ul > li, välj endast underliggande listan och öppna den. Vi vill inte
-    // ha med undermenyer vilket är varför selektorn enbart väljer ul-taggen som är ett direkt barn till li-taggen.
-    $('nav ul li').hover(function() {
-        $(this).children('ul').stop().slideDown(100);
-    }, function() {
-        $(this).children('ul').stop().slideUp(100);
-    }); // end hover
+  // För första 'nivån' av menyn nav > ul > li, välj endast underliggande listan och öppna den. Vi vill inte
+  // ha med undermenyer vilket är varför selektorn enbart väljer ul-taggen som är ett direkt barn till li-taggen.
+  $('nav ul li').hover(function() {
+    $(this).children('ul').stop().slideDown(100);
+  }, function() {
+    $(this).children('ul').stop().slideUp(100);
+  }); // end hover
 
-    // För andra nivån i menyn nav > ul > li > ul > li, välj undermenyn och öppna den.
-    $('nav ul li ul li').hover(function() {
-        $(this).children('ul').stop().slideDown(100);
-    }, function() {
-        $(this).children('ul').stop().slideUp(100);
-    }); // end hover
+  // För andra nivån i menyn nav > ul > li > ul > li, välj undermenyn och öppna den.
+  $('nav ul li ul li').hover(function() {
+    $(this).children('ul').stop().slideDown(100);
+  }, function() {
+    $(this).children('ul').stop().slideUp(100);
+  }); // end hover
 
-    // Toggla klassen som ändrar färg på listelementet.
-    $('nav li').hover(function() {
-        $(this).toggleClass('hover');
-    }, function() {
-        $(this).toggleClass('hover');
-    });
-
-
+  // Toggla klassen som ändrar färg på listelementet.
+  $('nav li').hover(function() {
+    $(this).toggleClass('hover');
+  }, function() {
+    $(this).toggleClass('hover');
+  });
 }); // end ready
