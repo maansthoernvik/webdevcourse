62dedcd84307af512e5876f8e60d45a015dac077
MTH <thornvik.mans@gmail.com>
2018-12-29 21:08:20 +0100

    Fixed 3.4.4 and validated the result

--- 3.4.4/script.js
+++ 3.4.4/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-    // Sätt alla element med en title-attribut att visa tooltip.
-    $('[title]').tooltip();
+  // Sätt alla element med en title-attribut att visa tooltip.
+  $('[title]').tooltip();
 
-    // Ändra content på var och en av textbitarna som ska visa en tooltip att hämta visningselementet från en template.
-    $('#jsTooltipTarget').tooltip({
-        content: $('#jsTooltipTemplate').html()
-    });
+  // Ändra content på var och en av textbitarna som ska visa en tooltip att hämta visningselementet från en template.
+  $('#jsTooltipTarget').tooltip({
+    content: $('#jsTooltipTemplate').html()
+  });
 
-    $('#htmlTooltipTarget').tooltip({
-        content: $('#htmlTooltipTemplate').html()
-    });
+  $('#htmlTooltipTarget').tooltip({
+    content: $('#htmlTooltipTemplate').html()
+  });
 
-    $('#cssTooltipTarget').tooltip({
-        content: $('#cssTooltipTemplate').html()
-    });
+  $('#cssTooltipTarget').tooltip({
+    content: $('#cssTooltipTemplate').html()
+  });
 
-    /*
-    Tabbade paneler
-     */
-    // Välj diven med klassen 'tabbedPanels' och gör om den till tabbade paneler från jqeury UI.
-    $('.tabbedPanels').tabs({
-        collapsible: true,  // Så att alla paneler kan döljas
-        heightStyle: 'fill' // Sätter höjden på panelerna efter containerns höjd.
-    });
+  /*
+  Tabbade paneler
+   */
+  // Välj diven med klassen 'tabbedPanels' och gör om den till tabbade paneler från jqeury UI.
+  $('.tabbedPanels').tabs({
+    collapsible: true,  // Så att alla paneler kan döljas
+    heightStyle: 'fill' // Sätter höjden på panelerna efter containerns höjd.
+  });
 });
